58d59b417b86cad521af33f542efcd457ff9687b
jacobtebben-ui <jacobtebben-ui@://github.com>
2026-09-25 18:15:24 -0400

    Complete e-plant shopping cart project features and reducers

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -20,7 +21,7 @@ function ProductList({ onHomeClick }) {
         }));
       };
       
-
+    const totalQuantity = useSelector(state => state.cart.items.reduce((total, item) => total + item.quantity, 0));
 
     const plantsArray = [
         {
